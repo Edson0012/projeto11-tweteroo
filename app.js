fc7696a971e8e5811f2eb08ca8b00fa053513fde
Edson0012 <edsonewertonrs1@gmail.com>
2022-08-26 15:55:25 -0300

    concluido requisitos de Post/tweets

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const user = [];
+const users = [];
 
-const twitter = {
-    username: "",
-    tweet: "",
-};
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    const users = req.body;
-    user.push(users);
-    res.send(users);
-    console.log("OK");
+    const user = req.body;
+    users.push(user);
+    res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send("OK");
 });
 
 app.listen(5000, () => console.log("listen 5000 on"));

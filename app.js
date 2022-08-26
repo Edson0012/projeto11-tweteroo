5194183113f9ae29c0455db2ca90447fea48ab89
Edson0012 <edsonewertonrs1@gmail.com>
2022-08-26 15:19:29 -0300

    requisito POST/sign-up concluido

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const user = [];
+
+const twitter = {
+    username: "",
+    tweet: "",
+};
+
+app.post("/sign-up", (req, res) => {
+    const users = req.body;
+    user.push(users);
+    res.send(users);
+    console.log("OK");
+});
+
+app.listen(5000, () => console.log("listen 5000 on"));
